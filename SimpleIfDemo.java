35bb3306305a8c0f4658f72d4a7e618105f3cfce
TanzeelaBanu <tanzeelatanz05@gmail.com>
2024-11-20 19:19:54 +0530

    Add files via upload

---
+++ SimpleIfDemo.java
@@ -0,0 +1,16 @@
+package tanzeela;
+
+public class SimpleIfDemo 
+{
+	public static void main(String[] args) 
+	{	
+		System.out.println("Start");
+		int number = 15;
+		if(number <= 10)
+		{
+			System.out.println(number + " is lesser then or equal to 10");
+		}
+		System.out.println("End");
+	}
+}
+
